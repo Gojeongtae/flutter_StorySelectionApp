7b999741776030ee88821d1098ee158d8dafeeb2
JeongTaeGo <giwigi@naver.com>
2024-01-13 15:02:22 +0900

    add selection

--- lib/main.dart
+++ lib/main.dart
@@ -26,17 +26,31 @@ class _MyHomePageState extends State<MyHomePage> {
   bool showText = false;
   bool showButton = false;
   bool showImage = true;
+  bool showChoices = false;
+  String selectedPerson = '';
 
   @override
   void initState() {
     super.initState();
     dataItems = Data.dataItems;
-    _showNextState();
+    _initNextState();
   }
+
+  void _initNextState() {
+    // 3초 뒤에 이미지가 반투명해지고 글이 나타남
+    Future.delayed(Duration(seconds: 3), () {
+      setState(() {
+        showImage = false;
+        showText = true;
+      });
+      _showButton();
+    });
+  }
+
 
   void _showNextState() {
     // 3초 뒤에 이미지가 반투명해지고 글이 나타남
-    Future.delayed(Duration(seconds: 3), () {
+    Future.delayed(Duration(seconds: 0), () {
       setState(() {
         showImage = false;
         showText = true;
@@ -61,6 +75,35 @@ class _MyHomePageState extends State<MyHomePage> {
       showImage = true;
       showText = false;
       showButton = false;
+      showChoices = false; // 선택지를 다시 숨김
+      _showNextState();
+    });
+  }
+
+  void _showChoices() {
+    // 선택지를 보여주도록 설정
+    setState(() {
+      showButton = false;
+      showChoices = true;
+    });
+  }
+
+  void _selectChoice(String choice) {
+    // 선택한 정보를 반영하여 2번째 데이터의 텍스트를 출력
+    setState(() {
+      selectedPerson = choice;
+      _showNewStory();
+    });
+  }
+
+  void _showNewStory() {
+    // 선택된 정보를 반영하여 2번째 데이터의 텍스트를 출력
+    setState(() {
+      currentIndex = 1; // 2번째 데이터로 이동
+      showImage = false;
+      showText = false;
+      showButton = false;
+      showChoices = false; // 선택지를 다시 숨김
       _showNextState();
     });
   }
@@ -69,14 +112,14 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Image and Text Carousel'),
+        title: Text('스토리'),
       ),
       body: Center(
         child: Stack(
           alignment: Alignment.center,
           children: [
             // 배경 이미지
-            AnimatedOpacity(
+            AnimatedOpacity(  //showImage가 true일 때, 이미지 보이도록
               opacity: showImage ? 1.0 : 0.5,
               duration: Duration(seconds: 1),
               child: Container(
@@ -90,18 +133,22 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             ),
-            // 글
-            AnimatedOpacity(
-              opacity: showText ? 1.0 : 0.0,
-              duration: Duration(seconds: 1),
+            // 글 또는 선택지 표시
+            AnimatedOpacity( //showText가 true일 때, 글 보이도록
+              opacity: (showText || showChoices) ? 1.0 : 0.0,
+              duration: Duration(seconds: 0),
               child: Positioned(
                 top: 16,
                 left: 16,
                 right: 16,
                 child: Container(
                   padding: EdgeInsets.all(16),
-                  child: Text(
-                    dataItems[currentIndex].text,
+                  child: showChoices
+                      ? _buildChoicesWidget()
+                      : Text(
+                    currentIndex == 1
+                        ? dataItems[currentIndex].text.replaceAll('{personName}', selectedPerson)
+                        : dataItems[currentIndex].text,
                     style: TextStyle(
                       fontSize: 16,
                       color: Colors.black,
@@ -117,8 +164,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 bottom: 16,
                 right: 16,
                 child: ElevatedButton(
-                  onPressed: () {
-                    _nextItem();
+                  onPressed: () { _showChoices();
                   },
                   child: Text('다음으로'),
                 ),
@@ -126,6 +172,38 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildChoicesWidget() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            _selectChoice('구현준');
+          },
+          child: Text('구현준'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            _selectChoice('김수찬');
+          },
+          child: Text('김수찬'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            _selectChoice('이승원');
+          },
+          child: Text('이승원'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            _selectChoice('오해람');
+          },
+          child: Text('오해람'),
+        ),
+      ],
     );
   }
 }
